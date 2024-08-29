976884250568f2ed6678c8ca4fc98dc9eb3be20f
meemkoo <guy99992@gmail.com>
2024-08-28 23:36:51 -0500

    Fixed

--- addons/menuthings/screencontroller.gd
+++ addons/menuthings/screencontroller.gd
@@ -8,26 +8,10 @@ var current_screen: int = 0
 
 func _ready() -> void:
     for screen in screens:
-        setstate(screen, false)
-    setstate(screens[current_screen], true)
-
-
-func getallnodes(node: Node, state: bool):
-    for N in node.get_children():
-        if N.get_child_count() > 0:
-            getallnodes(N, state)
-        else:
-            N.visible = state
-            N.set_process(state)
-
-
-func setstate(node: Node, state: bool):
-    node.visible = state
-    node.set_process(state)
-    getallnodes(node, state)
-
+        screen.visible = false
+    screens[current_screen].visible = true
 
 func switch(screen: Screen):
-    setstate(screens[current_screen], false)
+    screens[current_screen].visible = false
     current_screen = screens.find(screen)
-    setstate(screens[current_screen], true)
+    screen.visible = true
